675a649f61fe9620b094d7687440189bccfb507f
Amen Jlili <amen@bluebyte.biz>
2023-01-19 09:39:29 -0800

    #1 Added swDescriptiveFileWarningError_e

--- BlueByte.SOLIDWORKS.SDK.Core/Enums/swDescriptiveFileWarningError_e.cs
+++ BlueByte.SOLIDWORKS.SDK.Core/Enums/swDescriptiveFileWarningError_e.cs
@@ -6,29 +6,29 @@ namespace BlueByte.SOLIDWORKS.SDK.Core.Enums
     [Flags]
     public enum swDescriptiveFileWarningError_e
     {
-        [Description()]
+        [Description("There was a rebuild error.")]
         swFileSaveWarning_RebuildError = 1,
-        [Description()]
+        [Description("This file needs a rebuild.")]
         swFileSaveWarning_NeedsRebuild = 2,
-        [Description()]
+        [Description("The views in this file needs an update.")]
         swFileSaveWarning_ViewsNeedUpdate = 4,
-        [Description()]
+        [Description("The animator needs to solve.")]
         swFileSaveWarning_AnimatorNeedToSolve = 8,
-        [Description()]
+        [Description("The animator feature edits.")]
         swFileSaveWarning_AnimatorFeatureEdits = 16,
-        [Description()]
+        [Description("Edrawings bad selection.")]
         swFileSaveWarning_EdrwingsBadSelection = 32,
-        [Description()]
+        [Description("The animator light edits.")]
         swFileSaveWarning_AnimatorLightEdits = 64,
-        [Description()]
+        [Description("Animator camera views.")]
         swFileSaveWarning_AnimatorCameraViews = 128,
-        [Description()]
+        [Description("Animator section views.")]
         swFileSaveWarning_AnimatorSectionViews = 256,
-        [Description()]
+        [Description("There is a missing OLE object.")]
         swFileSaveWarning_MissingOLEObjects = 512,
-        [Description()]
+        [Description("The file was opened in viewonly mode.")]
         swFileSaveWarning_OpenedViewOnly = 1024,
-        [Description()]
+        [Description("Invalid XML.")]
         swFileSaveWarning_XmlInvalid = 2048
     }
 }
